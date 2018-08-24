4873f2b21d38e7ae79ae0711c969af12402eec05
boris.zaikin <boris.zaikin@re-flekt.com>
2018-08-24 14:43:01 +0200

    Refactor Startup; Add file operation filter;

---
+++ src/AzureFileAPI/AzureFileAPI/Configuration/IocConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AzureFileAPI.Data.AzureFileStorage;
+using AzureFileAPI.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AzureFileAPI.Configuration
+{
+    public static class IoCConfiguration
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            // IoC configuration
+            services.AddTransient<IAzureFileService, AzureFileService>();
+            services.AddTransient<IAzureFileStorageManager, AzureFileStorageManager>();
+            services.AddTransient<IBlobFileUploadUtility, BlobFileUploadUtility>();
+        }
+    }
+}
--- src/AzureFileAPI/AzureFileAPI/Startup.cs
+++ src/AzureFileAPI/AzureFileAPI/Startup.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.IO;
-using AzureFileAPI.Data.AzureFileStorage;
-using AzureFileAPI.Models.Configuration;
-using AzureFileAPI.Services;
+﻿using AzureFileAPI.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Swashbuckle.AspNetCore.Swagger;
 
 namespace AzureFileAPI
 {
@@ -24,34 +18,9 @@ namespace AzureFileAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<AzureStorageSettings>(this.Configuration.GetSection("ConnectionStrings:StorageConnection"));
-
-            // Swagger API documentation
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1",
-                    new Info
-                    {
-                        Title = "Azure File Api",
-                        Version = "v1.0",
-                        Description = "Azure BLOB File Management",
-                        TermsOfService = "TODO: Add Terms of service",
-                        Contact = new Contact
-                        {
-                            Name = "Boris Zaikin"
-                        },
-                        License = new License
-                        {
-                            Name = "MIT License",
-                            Url = "https://opensource.org/licenses/MIT"
-                        }
-                    });
-            });
-
-            // IoC configuration
-            services.AddTransient<IAzureFileService, AzureFileService>();
-            services.AddTransient<IAzureFileStorageManager, AzureFileStorageManager>();
-            services.AddTransient<IBlobFileUploadUtility, BlobFileUploadUtility>();
+            ConfigSettingConfiguration.ConfigureServices(services, Configuration);
+            SwaggerConfiguration.ConfigureServices(services);
+            IoCConfiguration.ConfigureServices(services);
 
             services.AddMvc();
         }
@@ -64,16 +33,7 @@ namespace AzureFileAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            // Swagger API documentation
-            var option = new RewriteOptions();
-            option.AddRedirect("^$", "swagger");
-            app.UseRewriter(option);
-
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Azure File Api v1.0");
-            });
+            SwaggerConfiguration.Configure(app);
 
             app.UseMvc();
         }
